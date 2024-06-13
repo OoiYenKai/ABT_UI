8f5c5b4f9768e23169420e7652ee1265cac43a9c
OoiYenKai <yenkai.22@ichat.sp.edu.sg>
2024-06-13 16:26:43 +0800

    added comments

--- scripts/run_ABT_UI.sh
+++ scripts/run_ABT_UI.sh
@@ -6,6 +6,7 @@ cd '/home/yenkai/Documents/CAG_Project'
 # source the virtual environment
 source myenv/bin/activate
 
+# Run the app
 textual run --dev ABT_UI.py
 
 # When the app is not running anymore, bring down the CAN interface
