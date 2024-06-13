06abd88d13397a2fb0bd2b949088314e181b1d6d
OoiYenKai <yenkai.22@ichat.sp.edu.sg>
2024-06-13 16:01:28 +0800

    Fixed the error of 'no such directory'

--- scripts/setup_UI.sh
+++ scripts/setup_UI.sh
@@ -18,13 +18,24 @@ pip3 install textual
 pip3 install textual-dev
 pip3 install python-can
 
-# Make the scripts executable
-chmod +x '/home/yenkai/Documents/CAG_Project/scripts/can_setup.sh'
-chmod +x '/home/yenkai/Documents/CAG_Project/scripts/run_ABT_UI.sh'
-chmod +x '/home/yenkai/Documents/CAG_Project/scripts/setup_UI.sh'
-chmod +x '/home/yenkai/Documents/CAG_Project/ABT_UI.py'
-chmod +x '/home/yenkai/Documents/CAG_Project/decoding.py'
+# Get the directory where this script resides
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SCRIPTS=(
+    "$BASE_DIR/scripts/can_setup.sh"
+    "$BASE_DIR/scripts/run_ABT_UI.sh"
+    "$BASE_DIR/scripts/setup_UI.sh"
+    "$BASE_DIR/ABT_UI.py"
+    "$BASE_DIR/decoding.py"
+)
+
+# Make each script executable
+for script in "${SCRIPTS[@]}"; do
+    chmod +x "$script"
+done
+
+echo "Scripts have been made executable."
 
 # Create a symbolic link to allow script to be run from anywhere
-sudo ln -s /home/yenkai/Documents/CAG_Project/decoding.py /usr/local/bin/decoding
-sudo ln -s /home/yenkai/Documents/CAG_Project/scripts/run_ABT_UI.sh /usr/local/bin/ABT_UI
+sudo ln -s $BASE_DIR/decoding.py /usr/local/bin/decoding
+sudo ln -s /$BASE_DIR/scripts/run_ABT_UI.sh /usr/local/bin/ABT_UI
