137d209f66dfbe853d1a00ae6cca04f6131bae45
YenKaiventi <yenkai.ooi@ventitechnologies.com>
2024-06-13 14:11:49 +0800

    Improved the CAN interface logic

--- decoding.py
+++ decoding.py
@@ -1,66 +1,79 @@
 #!/usr/bin/env python3
 import os
 import can
+import logging
 
 
-byte2 = 0.0
-byte3 = 0.0
-speed = 0.0
-battery = 0.0
+class CANInterface:
+    def __init__(self, interface='can0'):
+        self.interface = interface
+        self.bus = None
+
+    def setup(self):
+        os.system('scripts/./can_setup.sh')
+        self.bus = can.interface.Bus(channel=self.interface, bustype='socketcan')
+
+    def send_initial_message(self):
+        initialize_msg = can.Message(arbitration_id=0x123, data=[0xa0, 0xb1, 0xc2, 0xd3, 0xe4, 0xf5, 0xf6, 0xff], is_extended_id=False)
+        try:
+            self.bus.send(initialize_msg)
+            print(f"Message sent on {self.interface}")
+        except can.CanError:
+            print('Message was not sent')
+
+    def receive_message(self, arbitration_id, timeout=0.5):
+        try:
+            while True:
+                msg = self.bus.recv(timeout)
+                if msg is None:
+                    return None
+                if msg.arbitration_id == arbitration_id:
+                    return msg
+        except KeyboardInterrupt:
+            self.shutdown()
+            print('\n\rKeyboard interrupt received. Exiting.')
+            exit()
+
+    def shutdown(self):
+        self.bus.shutdown()
+        os.system(f'sudo ifconfig {self.interface} down')
+        logging.info(f'CAN interface {self.interface} shutdown completed.')
 
 
-os.system('scripts/./can_setup.sh')
-
-can_interface = 'can0'
-bus = can.interface.Bus(channel=can_interface, bustype='socketcan') #socketcan native
-
-initialize_msg = can.Message(arbitration_id=0x123, data=[0xa0, 0xb1, 0xc2, 0xd3, 0xe4, 0xf5, 0xf6, 0xff], is_extended_id=False)
-
-
-try:
-    #sending an initialization message
-    bus.send(initialize_msg)
-    print("Message sent on {}".format(can_interface))
-except can.CanError:
-    print('message was not sent')
+def decoding_speed(msg):
+    byte2 = hex(msg.data[1]).replace("0x", "")
+    byte3 = hex(msg.data[2]).replace("0x", "")
+    xSpeed = byte3 + byte2  # speed in hexadecimal
+    dSpeed = int(xSpeed, 16)  # speed in decimal
+    speed = dSpeed / 256  # speed in km/h
+    return "{:.2f}".format(speed)
 
 
-def decoding_speed():
-    try:
-        while True:
-            msg = bus.recv(0.5)
-            if msg is None:
-                break
-            if msg.arbitration_id == 0x10FEF1C8:
-                byte2 = hex(msg.data[1])
-                byte3 = hex(msg.data[2])
-                byte2 = byte2.replace("0x", "")
-                #using little endian format
-                xSpeed = byte3 + byte2 #speed in hexadecimal
-                dSpeed = int(xSpeed, 16) #speed in decimal
-                speed = dSpeed/256 #speed in km/h
-                #print(speed)
-                return "{:.2f}".format(speed)       
-    except KeyboardInterrupt:
-        bus.shutdown()
-        os.system('sudo ifconfig can0 down')
-        print('\n\rKeyboard interrupt received. Exiting.')
-        exit()
+def decoding_battery(msg):
+    byte2 = hex(msg.data[1]).replace("0x", "")
+    dbattery = int(byte2, 16)  # battery level in decimal
+    battery = dbattery * 0.4  # battery level in %
+    return "{:.2f}".format(battery)
 
-def decoding_battery():
-    try:
-        while True:
-            msg = bus.recv(0.5)
-            if msg is None:
-                break
-            if msg.arbitration_id == 0x10FEFCC8:
-                byte2 = hex(msg.data[1])
-                dbattery = int(byte2, 16) #speed in decimal
-                battery = dbattery * 0.4 #battery level in %
-                #print(battery)
-                return "{:.2f}".format(battery)       
-    except KeyboardInterrupt:
-        bus.shutdown()
-        os.system('sudo ifconfig can0 down')
-        print('\n\rKeyboard interrupt received. Exiting.')
-        exit()
+def main():
+    can_interface = CANInterface()
+    can_interface.setup()
+
+    can_interface.send_initial_message()
+
+    speed_msg_id = 0x18FEF1C8
+    battery_msg_id = 0x18FEFCC8
+
+    while True:
+        speed_msg = can_interface.receive_message(speed_msg_id)
+        if speed_msg:
+            print("Speed:", decoding_speed(speed_msg))
+
+        battery_msg = can_interface.receive_message(battery_msg_id)
+        if battery_msg:
+            print("Battery Level:", decoding_battery(battery_msg))
+
+
+
+if __name__ == "__main__":
+    main()
